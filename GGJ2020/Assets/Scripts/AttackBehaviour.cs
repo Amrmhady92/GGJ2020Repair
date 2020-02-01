77bff14220a98185b7f31bf35fed56b42a48ce10
Amr hero <amrmhady92@gmail.com>
2020-02-01 14:43:22 +0100

    adjustments

---
+++ GGJ2020/Assets/Scripts/AttackBehaviour.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public abstract class AttackBehaviour : MonoBehaviour
+{
+    public int attackDamage = 1;
+    public float attackSpeed = 0.3f;
+    public Action onAttackHit;
+
+    public abstract void Attack();
+    public abstract void OnAttackHit();
+}

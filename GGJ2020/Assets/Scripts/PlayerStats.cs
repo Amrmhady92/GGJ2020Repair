4b30b3acae0b64577cef23da103b614fcee693b5
Amr hero <amrmhady92@gmail.com>
2020-02-01 20:25:10 +0100

    Player Stats changes

--- GGJ2020/Assets/Scripts/PlayerStats.cs
+++ GGJ2020/Assets/Scripts/PlayerStats.cs
@@ -5,11 +5,44 @@ using UnityEngine;
 [CreateAssetMenu(menuName = ("SOs/PlayerStats"))]
 public class PlayerStats : ScriptableObject
 {
+
+    System.Action onPlayerDead;
+    System.Action onPlayerLoseHP;
+
     public int playerNumber = 1;
-    public int playerHP = 10;
-    public int playerMaxHP = 10;
+    private int playerHP = 10;
+    private int playerMaxHP = 10;
 
     public bool isPlaying = false;
 
+    public int PlayerHP
+    {
+        get
+        {
+            return playerHP;
+        }
+
+        set
+        {
+            if (playerHP != value)
+            {
+                onPlayerLoseHP?.Invoke();
+            }
+            playerHP = Mathf.Max(0, value);
+            if(playerHP <= 0)
+            {
+                onPlayerDead?.Invoke();
+            }
+
+        }
+    }
+
+    public int PlayerMaxHP
+    {
+        get
+        {
+            return playerMaxHP;
+        }
+    }
 }
 

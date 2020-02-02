c0a17603497a9bf59d5520c21f64d3a13ffd7e1c
Peace <kbarisbicak@hotmail.com>
2020-02-02 13:15:56 +0100

    Orbiting Planets

---
+++ GGJ2020/Assets/Scripts/Orbit.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Orbit : MonoBehaviour
+{
+    public GameObject rotate_around_object_;
+    public float orbit_speed_;
+
+    private void Update()
+    {
+        transform.RotateAround(rotate_around_object_.transform.position, Vector3.back, orbit_speed_ * Time.deltaTime);
+    }
+}
